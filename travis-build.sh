09500edd1fb882d3b862fe835a23e0e0010e6bc0
Olli Janatuinen <olli.janatuinen@gmail.com>
2020-08-30 17:37:39 +0300

    Travis test: build without github token

--- travis-build.sh
+++ travis-build.sh
@@ -7,10 +7,10 @@ if [ -z "$TRAVIS_BRANCH" ]; then
   exit 1
 fi
 # Check essential envs
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "ABORTING: env GITHUB_TOKEN is missing"
-  exit 1
-fi
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   echo "ABORTING: env GITHUB_TOKEN is missing"
+#   exit 1
+# fi
 
 # verbose logging
 set -x
@@ -25,9 +25,9 @@ docker-compose build
 docker-compose run builder
 
 # deploy to GitHub releases
-export GIT_TAG=v$BUILD_NR
-export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
-curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
-unzip ghr.zip
-./ghr --version
-./ghr --debug -u DieterReuter -b "$GIT_RELTEXT" $GIT_TAG builds/$BUILD_NR/
+# export GIT_TAG=v$BUILD_NR
+# export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
+# curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
+# unzip ghr.zip
+# ./ghr --version
+# ./ghr --debug -u DieterReuter -b "$GIT_RELTEXT" $GIT_TAG builds/$BUILD_NR/
